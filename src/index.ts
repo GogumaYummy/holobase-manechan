b20f99ae488f0828e7b49007a4f1e18af33ec824
GogumaYummy <sora1211x@gmail.com>
2026-06-07 19:29:24 +0900

    chore: 봇 클라이언트에 커스텀 로거 적용

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import { SapphireClient } from "@sapphire/framework";
-import { GatewayIntentBits } from "discord.js";
+import { SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+import { WinstonLogger } from './winston-logger.js';
 
-const client = new SapphireClient({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
+const client = new SapphireClient({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+	logger: { instance: new WinstonLogger() }
+});
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+async function main() {
+  try {
+    await client.login(process.env.DISCORD_BOT_TOKEN);
+  }
+  catch (error) {
+    client.logger.error(error);
+    process.exit(1);
+  }
+}
+
+main();
